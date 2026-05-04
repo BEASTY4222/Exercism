20f4110225821dd9017f942206742021a8fc279a
ivangeorgiev181@gmail.com <ivangeorgiev181@gmail.com>
2026-05-04 19:57:00 +0300

    RollTheDie DONE

--- hello-world/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ hello-world/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d1402571f49c06c41ebbda9fe0ba098abbe874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a3ca9ad280a453c94d27ff1d3cf602ef93fff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
