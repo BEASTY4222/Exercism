f0a0886be40246c0927290ee93bad1016500c241
Ivan Georgiev School <ivangeorgiev181@gmail.com>
2026-05-14 13:18:30 +0300

    resistor-color DONE

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -2,11 +2,24 @@ public static class ResistorColor
 {
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        switch (color)
+        {
+            case "black": return 0;
+            case "brown": return 1;
+            case "red": return 2;
+            case "orange": return 3;
+            case "yellow": return 4;
+            case "green": return 5;
+            case "blue": return 6;
+            case "violet": return 7;
+            case "grey": return 8; 
+            case "white": return 9;
+            default : return 0;
+        }
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return new string[]{"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
     }
 }
