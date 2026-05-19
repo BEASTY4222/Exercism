cc0ff30165327fe53497a1077ec8cb54a216ce0e
Ivan Georgiev School <ivangeorgiev181@gmail.com>
2026-05-19 11:20:51 +0300

    Strain DONE actuall push

--- strain/Strain.cs
+++ strain/Strain.cs
@@ -2,11 +2,21 @@ public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        List<T> result = new List<T>();
+        foreach (var elem in collection)
+        {
+            if (predicate(elem)) result.Add(elem);
+        }
+        return result;
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        List<T> result = new List<T>();
+        foreach (var elem in collection)
+        {
+            if (!predicate(elem)) result.Add(elem);
+        }
+        return result;    
     }
 }
