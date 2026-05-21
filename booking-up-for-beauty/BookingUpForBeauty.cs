4017de34ef4be4d472f5d520ef56a18f7ca97368
Ivan Georgiev <ivangeorgiev181@gmail.com>
2026-05-21 18:07:47 +0300

    booking-up-for-beauty

---
+++ booking-up-for-beauty/BookingUpForBeauty.cs
@@ -0,0 +1,29 @@
+static class Appointment
+{
+    public static DateTime Schedule(string appointmentDateDescription)
+    {
+        return DateTime.Parse(appointmentDateDescription);
+    }
+
+    public static bool HasPassed(DateTime appointmentDate)
+    {
+        if(appointmentDate > DateTime.Now) return false;
+        return true;
+    }
+
+    public static bool IsAfternoonAppointment(DateTime appointmentDate)
+    {
+        if(appointmentDate.Hour >= 12 && appointmentDate.Hour < 18) return true;
+        return false;
+    }
+
+    public static string Description(DateTime appointmentDate)
+    {
+        return $"You have an appointment on {appointmentDate:M/d/yyyy h:mm:ss tt}.";
+    }
+
+    public static DateTime AnniversaryDate()
+    {
+        return new DateTime(2026, 9, 15, 0, 0, 0);
+    }
+}
