dbdfa0c72cbb3f7ad008ae3b7640cacb79ea849d
Ivan Georgiev <ivangeorgiev181@gmail.com>
2026-05-21 18:59:45 +0300

    need-for-speed DONE

--- need-for-speed/NeedForSpeed.cs
+++ need-for-speed/NeedForSpeed.cs
@@ -1,34 +1,53 @@
 class RemoteControlCar
 {
     // TODO: define the constructor for the 'RemoteControlCar' class
+    private int speed;
+    private int batteryDrain;
+    private int distanceDriven;
+    private int battery;
+    public RemoteControlCar(int speed, int batteryDrain)
+    {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+        distanceDriven = 0;
+        battery = 100;
+    }
 
     public bool BatteryDrained()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+        return battery <= 0 || battery - batteryDrain < 0;
     }
 
     public int DistanceDriven()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+        return distanceDriven;
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if(!BatteryDrained())
+        {
+            battery -= batteryDrain;
+            distanceDriven += speed;    
+        }
+        else return;
     }
 
     public static RemoteControlCar Nitro()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+        return new RemoteControlCar(50, 4);
     }
 }
 
 class RaceTrack
 {
     // TODO: define the constructor for the 'RaceTrack' class
+    private int distance;
+    public RaceTrack(int distance){ this.distance = distance; } 
 
     public bool TryFinishTrack(RemoteControlCar car)
     {
-        throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
+        while(car.DistanceDriven() <= distance && !car.BatteryDrained()) car.Drive();
+        return car.DistanceDriven() >= distance;
     }
 }
--- space-age/obj/Debug/net10.0/SpaceAge.AssemblyInfo.cs
+++ space-age/obj/Debug/net10.0/SpaceAge.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NeedForSpeed")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SpaceAge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693d6ad1864ba5ede6876546626e8d3e9d6b2adf")]
-[assembly: System.Reflection.AssemblyProductAttribute("NeedForSpeed")]
-[assembly: System.Reflection.AssemblyTitleAttribute("NeedForSpeed")]
+[assembly: System.Reflection.AssemblyProductAttribute("SpaceAge")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SpaceAge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyMetadata("Microsoft.Testing.Platform.Application", "true")]
 
